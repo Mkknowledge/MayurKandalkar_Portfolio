f200dc51e70ddf21bb605324e7a0ec34702f0280
Mayur Kandalkar <kmayur2124@gmail.com>
2018-08-04 14:30:02 +0530

    Integrated Edit Update Show Action for Portfolio Page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,5 +19,25 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_items = Portfolio.find(params[:id])
+  end
+
+  def update
+  	@portfolio_items= Portfolio.find(params[:id])
+
+  	respond_to do |format|
+      if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+   def show
+    @portfolio_items= Portfolio.find(params[:id])
+  end
+
 
 end
