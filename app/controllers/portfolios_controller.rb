d5fe0e03d982b580ed7daf6ab5ea053cb713199c
Mayur Kandalkar <kmayur2124@gmail.com>
2018-08-04 14:00:54 +0530

    Integreted Create action for Portfolio Items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,4 +6,18 @@ class PortfoliosController < ApplicationController
 	def new 
 		@portfolio_items = Portfolio.new
 	end
+
+	def create
+		    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
 end
