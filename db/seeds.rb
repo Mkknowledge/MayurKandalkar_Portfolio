a79869b53db5ca06ac37bcabb685f192b6fcb597
Mayur Kandalkar <kmayur2124@gmail.com>
2018-08-04 11:10:45 +0530

    Created Blogs via scaffold, Skills via model, Portfolio via resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog #{blog}",
+		body: "my name is Mayur"
+		)
+end
+	puts "10 Blog Post updated."
+
+5.times do |skill|
+	Skill.create!(
+		title: "My Skill #{skill}",
+		percent_utilized: 85
+		)
+end
+	puts "5 Skills added."
+	
